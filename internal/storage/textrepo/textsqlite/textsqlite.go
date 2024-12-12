8635d46ca47fd5ee9e7ceb4656837231780d080c
Andrei Markov <markov.andrey.v@gmail.com>
2024-12-12 16:07:38 +0300

    Add sqlite support

--- internal/storage/textrepo/textsqlite/textsqlite.go
+++ internal/storage/textrepo/textsqlite/textsqlite.go
@@ -127,3 +127,98 @@ func (s *Storage) GetSecret(ctx context.Context, userID, name string) (*text.Sec
 
 	return secret, nil
 }
+
+// ListSecrets returns a list of text secret entries from the storage.
+func (s *Storage) ListSecrets(ctx context.Context, userID string) ([]*text.Secret, error) {
+	query := `SELECT id, name, user_id, created_at, updated_at, version, metadata, salt, iv, location, checksum
+		FROM vault_texts
+		WHERE user_id = $1`
+
+	rows, err := s.db.QueryContext(ctx, query, userID)
+	if err != nil {
+		return nil, fmt.Errorf("db.QueryContext: %w", err)
+	}
+	defer rows.Close()
+
+	var secrets []*text.Secret
+
+	for rows.Next() {
+		var dbSecret textrepo.Secret
+
+		err := rows.Scan(&dbSecret.ID, &dbSecret.Name, &dbSecret.UserID, &dbSecret.CreatedAt, &dbSecret.UpdatedAt,
+			&dbSecret.Version, &dbSecret.Metadata, &dbSecret.Salt, &dbSecret.IV, &dbSecret.Location, &dbSecret.Checksum)
+		if err != nil {
+			return nil, fmt.Errorf("rows.Scan: %w", err)
+		}
+
+		var metadata map[string]string
+
+		err = json.Unmarshal([]byte(dbSecret.Metadata), &metadata)
+		if err != nil {
+			return nil, fmt.Errorf("failed to unmarshal metadata: %w", err)
+		}
+
+		info := text.NewContentInfo(dbSecret.Salt, dbSecret.IV, dbSecret.Location, dbSecret.Checksum)
+
+		secret, err := text.NewSecret(dbSecret.ID, dbSecret.Name, dbSecret.UserID, metadata,
+			dbSecret.CreatedAt, dbSecret.UpdatedAt, dbSecret.Version, info)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create text secret: %w", err)
+		}
+
+		secrets = append(secrets, secret)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("rows.Err: %w", err)
+	}
+
+	return secrets, nil
+}
+
+// UpdateSecret updates a text secret entry in the storage.
+func (s *Storage) UpdateSecret(ctx context.Context, secret *text.Secret) (*text.Secret, error) {
+	metadata, err := json.Marshal(secret.Metadata())
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal metadata: %w", err)
+	}
+
+	query := `UPDATE vault_texts
+		SET updated_at = $1, version = $2, metadata = $3, salt = $4, iv = $5, location = $6, checksum = $7
+		WHERE user_id = $8 AND name = $9`
+
+	_, err = s.db.ExecContext(ctx, query, secret.UpdatedAt(), secret.Version(), metadata,
+		secret.ContentInfo().Salt(), secret.ContentInfo().IV(), secret.ContentInfo().Location(),
+		secret.ContentInfo().Checksum(), secret.UserID(), secret.Name())
+	if err != nil {
+		return nil, fmt.Errorf("db.ExecContext: %w", err)
+	}
+
+	secr, err := s.GetSecret(ctx, secret.UserID(), secret.Name())
+	if err != nil {
+		return nil, fmt.Errorf("%w", err)
+	}
+
+	return secr, nil
+}
+
+// DeleteSecret deletes a text secret entry from the storage.
+func (s *Storage) DeleteSecret(ctx context.Context, userID, name string) error {
+	query := `DELETE FROM vault_texts WHERE user_id = $1 AND name = $2`
+
+	result, err := s.db.ExecContext(ctx, query, userID, name)
+	if err != nil {
+		return fmt.Errorf("db.ExecContext: %w", err)
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return fmt.Errorf("result.RowsAffected: %w", err)
+	}
+
+	if rowsAffected == 0 {
+		return textrepo.ErrSecretNotFound
+	}
+
+	return nil
+}
